95951c32b0784eac652a7ae2a6c2fd5f286b5dee
Chris Rhodes <ctrhodes@users.noreply.github.com>
2021-06-20 15:54:16 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,19 @@
 sinteractive
 module load ruby gcc/10.2.0
 
-# Install Ruby Gems to ~/gems:
+# Install Ruby Gems home to path:
 echo 'export GEM_HOME="$HOME/gems"' >> ~/.bashrc
 echo 'export PATH="$HOME/gems/bin:$PATH"' >> ~/.bashrc
 source ~/.bashrc
 
+# create new site
 gem install bundler jekyll
+jekyll new my-awesome-site
+cd my-awesome-site
+bundle exec jekyll serve
+
+# => Now browse to http://localhost:4000
+
+# serve existing site
+cd existing-site
+bundle exec jekyll serve
