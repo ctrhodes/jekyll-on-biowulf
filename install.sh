c1b69915f4995c3bb84d267a7f7acca2f8a9b399
Chris Rhodes <ctrhodes@users.noreply.github.com>
2021-06-20 15:58:49 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
 # to use browsers on Biowulf requires port forwarding. Easiest way to deal with this is use NoMachine or other vitual desktop and serve site to local host
 
-sinteractive
-module load ruby gcc/10.2.0
-
 # Install Ruby Gems home to path:
 echo 'export GEM_HOME="$HOME/gems"' >> ~/.bashrc
 echo 'export PATH="$HOME/gems/bin:$PATH"' >> ~/.bashrc
 source ~/.bashrc
+
+# start interactive session. as of writing, gcc/9.2.0 and gcc/10.2.0 versions work
+sinteractive
+module load ruby gcc/10.2.0
 
 # create new site
 gem install bundler jekyll
